58cb9dd523dbbbaf82e94a282b0822b796f021da
Sagi Bashari <sagi@myheritage.com>
2020-05-09 23:48:19 +0300

    test() each case separately

--- src/scrapers/visa-cal/converter.test.ts
+++ src/scrapers/visa-cal/converter.test.ts
@@ -2,9 +2,9 @@ import makeTestCases from './converter.test-data';
 import VisaCalTransactionConverter from './converter';
 
 describe('VisaCal converter test', () => {
-  test('convert-installments', () => {
-    const converter = new VisaCalTransactionConverter();
-    makeTestCases().forEach((tcase) => {
+  const converter = new VisaCalTransactionConverter();
+  makeTestCases().forEach((tcase, name) => {
+    test(name, () => {
       expect(converter.convertTransaction(tcase.input)).toEqual(tcase.expected);
     });
   });
